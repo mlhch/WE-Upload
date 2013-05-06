8d9f8a0d4f46f496a956630be088f29fc6edfd90
Ma Lian <malhch@gmail.com>
2013-05-07 02:20:19 +0800

    maki icon css added

--- style.php
+++ style.php
@@ -23,4 +23,8 @@ function cura_phase2_js_and_css() {
 	$src = CURAH2O_PLUGIN_URL . 'app/styles/font-awesome.min.css';
 	wp_register_style('cura-app-font-awesome-style', $src);
 	wp_enqueue_style('cura-app-font-awesome-style');
+
+	$src = CURAH2O_PLUGIN_URL . 'app/styles/maki-sprite.css';
+	wp_register_style('cura-app-maki-sprite-style', $src);
+	wp_enqueue_style('cura-app-maki-sprite-style');
 }
