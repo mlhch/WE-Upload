f17a60544069e5925eb318a933076a4050ba1e4b
Ma Lian <malhch@gmail.com>
2013-05-11 17:40:25 +0800

    update for get locations from curaGeoJSONLayer instead of server

--- app/scripts/curah2o/cura.leaflet.geojson.js
+++ app/scripts/curah2o/cura.leaflet.geojson.js
@@ -6,7 +6,9 @@
 		iconAnchor: [5, 2]
 	});
 
-	var lastLayer = null;
+	var lastLayer;
+	var locations;
+	var allLayers = [];
 
 	Cura.GeoJSON = L.GeoJSON.extend({
 		options: {
@@ -26,6 +28,7 @@
 					'<br />' + p.watershed_name].join(''));
 				layer.on('click', this.highlightIcon);
 				layer.on('click', this.onFeatureClick); // need layer to be the future 'this'
+				allLayers.push(layer);
 			},
 			highlightIcon: function() {
 				if (lastLayer) {
@@ -44,6 +47,68 @@
 			},
 			onFeatureClick: function() {}
 		},
+
+		locations: function() {
+			if (locations) {
+				return locations;
+			}
+
+			var map = {};
+			this.eachLayer(function(layer) {
+				var key = layer.feature.properties.watershed_name;
+				map[key] ? map[key]++ : (map[key] = 1)
+			});
+
+			locations = [{
+				id: "",
+				watershed_name: " - Select community group - "
+			}, {
+				id: 0,
+				watershed_name: "View All"
+			}];
+
+			var i = 1;
+			for (var key in map) {
+				locations.push({
+					id: i++,
+					watershed_name: key,
+					total: map[key]
+				})
+			}
+
+			return locations;
+		},
+
+		filterByCommunityGroup: function(groupId) {
+			if (groupId === "") {
+				this.clearLayers();
+			} else if (groupId === 0) {
+				allLayers.forEach(function(layer) {
+					this.addLayer(layer);
+				}, this);
+			} else {
+				var watershed_name = "";
+				this.locations().every(function(value) {
+					if (value.id == id) {
+						watershed_name = value.watershed_name;
+						return false;
+					}
+					return true;
+				});
+
+				allLayers.forEach(function(layer) {
+					if (layer.feature.properties.watershed_name == watershed_name) {
+						this.addLayer(layer);
+					} else {
+						this.removeLayer(layer);
+					}
+				}, this);
+			}
+		},
+
+		filterByProperties: function(propValue, propName) {
+
+		}
 	});
 
 	Cura.geoJson = function(geojson, options) {
